2d57196aa674a8dbb9557126de5c695a0f1af7f2
Amarankumar <136580994+Amarankumar@users.noreply.github.com>
2023-06-25 08:53:35 +0530

    Add files via upload

---
+++ reversedstring.py
@@ -0,0 +1,11 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+string =input('Enter a string:')     # "1234abcd"
+reversed_string = reverse_string(string)
+print('Reversed string is :',reversed_string)
+
+# output
+#
+# Enter a string: 1234abcd
+# Reversed string is : dcba4321
